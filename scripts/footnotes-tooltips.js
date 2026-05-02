32e060d134f5dd75d371465e95ae5e47a57c09c8
Daisuke Sugiyama <dsugiyama@others-step.dev>
2026-05-02 21:00:52 +0900

    XPath でなく CSS セレクターで十分だった

--- scripts/footnotes-tooltips.js
+++ scripts/footnotes-tooltips.js
@@ -1,15 +1,10 @@
 "use strict";
 
-// use ORDERED_NODE_SNAPSHOT_TYPE to ensure that items at the same index correspond to each other
-let footnoteRefs = document.evaluate("//a[@class='footnote']",
-	document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-let footnotes = document.evaluate("//div[@class='footnotes']//li/p",
-	document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+let footnoteRefs = document.querySelectorAll("a.footnote");
+let footnotes = document.querySelectorAll("div.footnotes li > p");
 
-for (let i = 0; i < footnotes.snapshotLength; i++) {
-	let refNode = footnoteRefs.snapshotItem(i);
-	let noteNode = footnotes.snapshotItem(i);
-	let text = noteNode.innerText;
+for (let i = 0; i < footnotes.length; i++) {
+	let text = footnotes[i].innerText;
 	text = text.substring(0, text.length - 2); // remove trailing space and symbol '↩'
-	refNode.setAttribute("title", text);
+	footnoteRefs[i].setAttribute("title", text);
 }
